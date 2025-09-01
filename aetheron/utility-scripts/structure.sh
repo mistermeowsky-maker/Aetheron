453f6ad5576ae08991fd42bda76b9cf63afa6dd5
mistermeowsky-maker <mister.meowsky@gmail.com>
2025-09-01 22:16:29 +0200

    Delete aetheron directory

--- aetheron/utility-scripts/structure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-BASE=~/aetheron
-echo "=== AETHERON STRUCTURE ===" > overview.txt
-tree -a $BASE >> overview.txt
-echo -e "\n=== SERVER-SETUP.SH ===" >> overview.txt
-cat $BASE/server-setup.sh >> overview.txt
-echo -e "\n=== COMMON.SH ===" >> overview.txt  
-cat $BASE/scripts/common.sh >> overview.txt
